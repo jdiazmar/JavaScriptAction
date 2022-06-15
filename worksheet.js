09d347e1b3c1eac7f22502b823e1d37533530a6a
Josmar Diaz <jdm.begreat28@gmail.com>
2022-06-15 16:07:58 -0400

    Completed Practice questions 1 and 2

---
+++ worksheet.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// console.log('Hello World!')
+
+// var dayOfWeek = 'Monday';
+// console.log(dayOfWeek)
+
+// var dayOfWeek = 'Friday';
+// console.log("I can't wait for Friday!")
+
+// let animalInput = (prompt("What is your favorite animal?"));
+// let colorInput = (prompt("What is your favorite color?"));
+// console.log(`I've never seen a ${colorInput} ${animalInput} before!`)
