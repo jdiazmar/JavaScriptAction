d0aa3041a0f346cf0ebe82f80e0c5ae8598dc340
Josmar Diaz <jdm.begreat28@gmail.com>
2022-06-15 17:05:44 -0400

    Working on Conditionals Problem

--- worksheet.js
+++ worksheet.js
@@ -1,13 +1,33 @@
 "use strict";
 
-// console.log('Hello World!')
+// Testing Webpage
+console.log('Hello World!')
 
-// var dayOfWeek = 'Monday';
-// console.log(dayOfWeek)
+// 1. Variables
+var dayOfWeek = 'Monday';
+console.log(dayOfWeek)
 
-// var dayOfWeek = 'Friday';
-// console.log("I can't wait for Friday!")
+var dayOfWeek = 'Friday';
+console.log("I can't wait for Friday!")
 
-// let animalInput = (prompt("What is your favorite animal?"));
-// let colorInput = (prompt("What is your favorite color?"));
-// console.log(`I've never seen a ${colorInput} ${animalInput} before!`)
+// 2. User Input
+let animalInput = (prompt("What is your favorite animal?"));
+let colorInput = (prompt("What is your favorite color?"));
+console.log(`I've never seen a ${colorInput} ${animalInput} before!`)
+
+// 3. Conditionals
+// Favorite Breakfast: Bacon, Egg, and Cheese sandwich with Ketchup!
+// Favorite Lunch: Chick-Fil-A Spicy sandwich with medium fries
+// Favorite Dinner: White rice, red beans, and stew chicken
+
+let timeOfDay = 1300;
+let timeToEat = ''
+
+    if(timeOfDay < 1200){
+        return timeToEat = 'Bacon, Egg, and Cheese sandwich with Ketchup';
+    }else if(timeOfDay >= 1200){
+        return timeToEat = 'Chick-Fil-A Spicy sandwich with medium fries';
+    }else
+    {
+        return timeToEat = 'White rice, red beans, and stew chicken';
+    }
