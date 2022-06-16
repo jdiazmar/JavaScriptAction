b753907c3a91a11b12ccf53bfa75340150c822fb
Josmar Diaz <jdm.begreat28@gmail.com>
2022-06-16 16:47:50 -0400

    Fixed Syntax Errors in JS File

--- worksheet.js
+++ worksheet.js
@@ -22,35 +22,37 @@ console.log(`I've never seen a ${colorInput} ${animalInput} before!`)
 // Favorite Dinner: White rice, red beans, and stew chicken
 
 let timeOfDay = 1300;
-let timeToEat = ''
+let timeToEatBreakfast = 'Breakfast: Bacon, Egg, and Cheese sandwich with Ketchup!'
+let timeToEatLucnch = 'Lunch: Chick-Fil-A Spicy sandwich with medium fries'
+let timeToEatDinner = 'Dinner: White rice, red beans, and stew chicken'
 
-if(timeOfDay < 1200){
-    return timeToEat = 'Bacon, Egg, and Cheese sandwich with Ketchup';
+if (timeOfDay < 1200){
+    console.log(timeToEatBreakfast);
 }else if(timeOfDay >= 1200){
-    return timeToEat = 'Chick-Fil-A Spicy sandwich with medium fries';
+    console.log(timeToEatLucnch);
 }else
 {
-    return timeToEat = 'White rice, red beans, and stew chicken';
+    console.log(timeToEatDinner);
 }
 
 // Random Number
 var randomNumber = Math.floor(Math.random() * 10 + 1); 
     if (randomNumber <= 2){
-        return console.log('Beatles')
+        console.log('Beatles')
     } else if (randomNumber = 3){
-        return console.log('Stones')
+        console.log('Stones')
     }else if (randomNumber = 4){
-        return console.log("Stones")
+        console.log("Stones")
     }else if (randomNumber = 5){
-        return console.log('Stones')
+        console.log('Stones')
     }else if (randomNumber = 6){
-        return console.log('Floyd')
+        console.log('Floyd')
     }else if (randomNumber = 7){
-        return console.log('Floyd')
+        console.log('Floyd')
     }else if (randomNumber = 8){
-        return console.log('Floyd')
+        console.log('Floyd')
     }else {
-        return console.log('Hendrix')
+        console.log('Hendrix')
     }
         
 
