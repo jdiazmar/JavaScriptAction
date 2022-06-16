c91ac4e5df90c5fbec3a07bd3c15e03cc6dda40b
Josmar Diaz <jdm.begreat28@gmail.com>
2022-06-15 20:52:59 -0400

    Finished Conditionals Problem By Working On Meals and Random Numbers

--- worksheet.js
+++ worksheet.js
@@ -16,6 +16,7 @@ let colorInput = (prompt("What is your favorite color?"));
 console.log(`I've never seen a ${colorInput} ${animalInput} before!`)
 
 // 3. Conditionals
+// Meals
 // Favorite Breakfast: Bacon, Egg, and Cheese sandwich with Ketchup!
 // Favorite Lunch: Chick-Fil-A Spicy sandwich with medium fries
 // Favorite Dinner: White rice, red beans, and stew chicken
@@ -23,11 +24,35 @@ console.log(`I've never seen a ${colorInput} ${animalInput} before!`)
 let timeOfDay = 1300;
 let timeToEat = ''
 
-    if(timeOfDay < 1200){
-        return timeToEat = 'Bacon, Egg, and Cheese sandwich with Ketchup';
-    }else if(timeOfDay >= 1200){
-        return timeToEat = 'Chick-Fil-A Spicy sandwich with medium fries';
-    }else
-    {
-        return timeToEat = 'White rice, red beans, and stew chicken';
+if(timeOfDay < 1200){
+    return timeToEat = 'Bacon, Egg, and Cheese sandwich with Ketchup';
+}else if(timeOfDay >= 1200){
+    return timeToEat = 'Chick-Fil-A Spicy sandwich with medium fries';
+}else
+{
+    return timeToEat = 'White rice, red beans, and stew chicken';
+}
+
+// Random Number
+var randomNumber = Math.floor(Math.random() * 10 + 1); 
+    if (randomNumber <= 2){
+        return console.log('Beatles')
+    } else if (randomNumber = 3){
+        return console.log('Stones')
+    }else if (randomNumber = 4){
+        return console.log("Stones")
+    }else if (randomNumber = 5){
+        return console.log('Stones')
+    }else if (randomNumber = 6){
+        return console.log('Floyd')
+    }else if (randomNumber = 7){
+        return console.log('Floyd')
+    }else if (randomNumber = 8){
+        return console.log('Floyd')
+    }else {
+        return console.log('Hendrix')
     }
+        
+
+
+
