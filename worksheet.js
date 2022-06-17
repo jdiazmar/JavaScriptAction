80cc55f7327c394e6a30680a223d23fd17d734e6
Josmar Diaz <jdm.begreat28@gmail.com>
2022-06-16 20:12:38 -0400

    Finished JavaScript Assignment

--- worksheet.js
+++ worksheet.js
@@ -68,10 +68,68 @@
 // }
 
 // For Loops #3
-let helloAndGoodbye = ['hello', 'goodbye', 'hello', 'goodbye', 'hello', 'goodbye', 'hello', 'goodbye', 'hello', 'goodbye']
-for(let i = 0; i<helloAndGoodbye.length; i++){
-    console.log(helloAndGoodbye[i])
-} 
+// let helloAndGoodbye = ['hello', 'goodbye', 'hello', 'goodbye', 'hello', 'goodbye', 'hello', 'goodbye', 'hello', 'goodbye']
+// for(let i = 0; i<helloAndGoodbye.length; i++){
+//     console.log(helloAndGoodbye[i])
+// } 
 
 
+// While Loops #1
+//  let favoriteMovie = 'The Sandlot'
+//     console.log(favoriteMovie)
+
+// Functions 
+// Example on why it won't work
+// var favoriteMovie = "The Sandlot"
+// function printMovieName(favoriteMovie){
+//     let results;
+//     results = printMovieName;
+//     return results
+// }
+// Favorite Band Function
+// function favoriteBand(){
+//     let result; 
+//     result = 'Fall Out Boy';
+//     return result;
+// }
+// let myResult = favoriteBand();
+// console.log(`${myResult} is my favorite band too!`)
+
+// Concert(parameters) Function **************
+// function concertDisplay(musicalAct){
+//     let myStreet = (prompt("Please Enter Your Street Address!")); 
+//     musicalAct = `It would be great is Joey Bada$$ played a show on ${myStreet}`;
+//     return musicalAct;
+// } 
+// console.log(concertDisplay)
+
+// Arrays #1 Desktop Items
+// const desktopItems = ["Desk", "Monitor", "Keyboard"];
+// console.log(desktopItems[1]);
+// // Adding a new item to the Array
+// desktopItems.push("Infinity Gauntlet");
+// console.log(desktopItems);
+// // Array Loop
+// var myDesktopItems = desktopItems.length;
+// for (var i = 0; i < myDesktopItems; i++){
+//     console.log(desktopItems[i])
+// }
+
+// Magic Number
+let magicNumber = 50;
+let guess = 0;
+let userInput = (prompt("Guess the Magic Number!"));
+
+
+while(userInput){
+    if (userInput < magicNumber){
+        console.log('Too Low!')
+    }else if(userInput > magicNumber){
+        console.log('Too High!')
+    }else if(_.inRange(userInput, 40, 60)){
+        console.log('Getting Warmer!')
+    }else{
+        console.log('Congratulations! You Guessed Correctly!')
+    }
+}
 
